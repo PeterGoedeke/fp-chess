2f3a709ef4911ffc97bf80f197edc1e4f711b6e5
PeterGoedeke <peterbgoedeke@gmail.com>
2020-06-30 18:54:26 +1200

    Added matrix manipulation helper functions

--- base.js
+++ base.js
@@ -7,6 +7,17 @@ const pointEq = p1 => p2 => p1.i == p2.i && p1.j == p2.j
 const merge = o1 => o2 => Object.assign({}, o1, o2)
 const prop = k => o => o[k]
 const pipe = (...fns) => x => [...fns].reduce((acc, f) => f(acc), x)
+
+const replace = arr => i => x => Object.assign([], arr, { [i]: x })
+const replace2d = (i, j) => x => mat => Object.assign([], mat, { [i]: replace(mat[i])(j)(x) })
+
+const replaceBetween = (i1, j1) => (i2, j2) => x => mat => {
+    if(pointEq({i: i1, j: j1})({i: i2, j: j2})) return mat
+
+    const v = getUnitVector(i1, j1)(i2, j2)
+    return replaceBetween(i1 + v[0], j1 + v[1])(i2, j2)(x)(replace2d(i1, j1)(x)(mat))
+}
+
 const pipeLog = (x, ...y) => (console.log(x, ...y), x)
 const andLog = (x, ...y) => (console.log(x, ...y), true)
 
@@ -17,6 +28,11 @@ module.exports = {
     merge,
     prop,
     pipe,
+
+    replace,
+    replace2d,
+    replaceBetween,
+
     pipeLog,
     andLog
 }
