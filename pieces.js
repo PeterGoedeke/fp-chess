b61cd48fab9b360c8181d92487ee33455869d2eb
PeterGoedeke <peterbgoedeke@gmail.com>
2020-07-02 16:50:02 +1200

    Added moves for the major pieces

--- pieces.js
+++ pieces.js
@@ -22,6 +22,44 @@ const checkPawnCapture = state => (i, j, dir) => mat => board.areDifferentTeam(s
 const checkEnpassant = state => (i, j, dir) => mat => state.enpassant && base.pointEq(state.enpassant)({i: i + dir, j})
     ? base.replace2d(i + dir, j + 1)(1)(mat)
     : mat
+
+const getRookMoves = state => (i, j) => mat => checkOrthogonalDirections(state)(i, j)(base.replace2d(i, j)('x')(mat))
+const getBishopMoves = state => (i, j) => mat => checkDiagonalDirections(state)(i, j)(base.replace2d(i, j)('x')(mat))
+
+const getQueenMoves = state => (i, j) => mat => base.pipe(
+    checkOrthogonalDirections(state)(i, j),
+    checkDiagonalDirections(state)(i, j)
+)(base.replace2d(i, j)('x')(mat))
+
+
+const checkOrthogonalDirections = state => (i, j) => mat => base.pipe(
+    checkDirection(state)(i, j, -1, 0),
+    checkDirection(state)(i, j, 1, 0),
+    checkDirection(state)(i, j, 0, -1),
+    checkDirection(state)(i, j, 0, 1)
+)(mat)
+
+const checkDiagonalDirections = state => (i, j) => mat => base.pipe(
+    checkDirection(state)(i, j, 1, 1),
+    checkDirection(state)(i, j, -1, -1),
+    checkDirection(state)(i, j, 1, -1),
+    checkDirection(state)(i, j, -1, 1)
+)(mat)
+    
+const checkDirection = state => (i, j, dI, dJ) => mat => board.areDifferentTeam(state)(i, j)(i + dI, j + dJ)
+    ? base.replace2d(i + dI, j + dJ)(1)(mat)
+    : board.squareIsEmpty(state)(i + dI, j + dJ)
+        ? checkDirection(state)(i, j, dI + base.scalarDirection(dI), dJ + base.scalarDirection(dJ))
+            (base.replace2d(i + dI, j + dJ)(1)(mat))
+        : mat
+
+const checkSquare = state => (i, j, dI, dJ) => mat => board.areDifferentTeam(state)(i, j)(i + dI, j + dJ) || board.getCodeAt(state)(i + dI, j + dJ) == 0
+    ? base.replace2d(i + dI, j + dJ)(1)(mat)
+    : mat
+
 module.exports = {
     pawn: getPawnMoves,
+    rook: getRookMoves,
+    bishop: getBishopMoves,
+    queen: getQueenMoves,
 }
