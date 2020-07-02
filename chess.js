69efef0bd784df159983b2643d170aa0aed00e72
PeterGoedeke <peterbgoedeke@gmail.com>
2020-07-02 20:55:42 +1200

    Game mostly playable in terminal

--- chess.js
+++ chess.js
@@ -1,60 +1,117 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+})
+
 const base = require('./base')
 const board = require('./board')
 
-const pawn = require('./pieces/pawn')
+const pieces = require('./pieces')
 
 const initialState = {
     board: [
-        ['R', 0, 0, 0, 'P', 0, 'p', 'r'],
-        ['N', 'P', 0, 0, 'p', 0, 0, 'n'],
+        ['R', 'P', 0, 0, 0, 0, 'p', 'r'],
+        ['N', 'P', 0, 0, 0, 0, 'p', 'n'],
         ['B', 'P', 0, 0, 0, 0, 'p', 'b'],
-        ['Q', 'P', 0, 0, 0, 0, 'p', 'q'],
+        ['Q', 'P', 0, 0, 0, 'N', 'r', 'q'],
         ['K', 'P', 0, 0, 0, 0, 'p', 'k'],
         ['B', 'P', 0, 0, 0, 0, 'p', 'b'],
         ['N', 'P', 0, 0, 0, 0, 'p', 'n'],
         ['R', 'P', 0, 0, 0, 0, 'p', 'r'],
     ],
-    whoseMove: 0,
-    enpassant: { i: 1, j: 4 }
+    whoseMove: 1,
+    enpassant: { i: 1, j: 4 },
+    gameState: {
+        whiteWin: false,
+        blackWin: false,
+        draw: false
+    }
 }
 
-const nextBoard = state => validMove(state)
-    ? applyMove(state)
+const nextBoard = state => validMove(state)(state.move)
+    ? applyMove(state)(state.move)
     : state.board
 
-// const next = spec({
-//     whoseMove: !base.prop('whoseMove'),
-//     board: nextBoard,
-//     move: clear,
-//     enpassant: nextEnpassant
-// })
-
 const clear = state => null
-const nextEnpassant = state => board.pieceToMove(state).toLowerCase() == 'p' // piece to move is a pawn
-    && base.pointDistance(state.move[0])(state.move[1]) == 2 // piece is being moved two places
-    ? state.move[1]
-    : null
+const nextEnpassant = state => state.move && validMove(state)(state.move)
+    ? board.pieceToMove(state) && board.pieceToMove(state).toLowerCase() == 'p' // piece to move is a pawn
+        && base.pointDistance(state.move[0])(state.move[1]) == 2 // piece is being moved two places
+        ? state.move[1]
+        : null
+    : state.enpassant
 
     // && state.move[0].j == 1 && state.whoseMove == 1 || state.move[0].j == 6 && state.whoseMove == 0 // 
 
-const validMove = state => state.move
-    ? !base.pointEq(state.move[0])(state.move[1])
-        ? getCodeAt(state)(...state.move[0])
+const validMove = state => move => !!move
+    ? !board.squareIsEmpty(state)(move[0].i, move[0].j) && !base.pointEq(move[0])(move[1])
+    && board.teamAtIs(state)(move[0].i, move[0].j)(state.whoseMove) && pieces.at(state)(move[0].i, move[0].j)(board.ZEROS)[move[1].i][move[1].j] == 1
+        ? !pieces.kingOfTeamChecked(base.merge(state)({board: applyMove(state)(move)}))(state.whoseMove)
         : false
     : false
 
 // check if black and if black then flip before pipeing otherwise just pipe
 
-const applyMove = state => base.pipe(
-    base.replace2d(...state.move[1])(board.pieceToMove(state)),
-    base.replace2d(...state.move[0])(0),
+const applyMove = state => move => base.pipe(
+    base.replace2d(move[1].i, move[1].j)(board.getCodeAt(state)(move[0].i, move[0].j)),
+    base.replace2d(move[0].i, move[0].j)(0),
     // check for en passant and apply it here
     // check for castling and apply it here
     // check for queening and apply it here
 )(state.board)
 
-function printBoard(board) {
-    console.log('----------------------')
-    board.forEach(column => console.log(column.join(', ')))
-    console.log('----------------------')
+const nextWhoseMove = state => validMove(state)(state.move)
+    ? !state.whoseMove
+    : state.whoseMove
+
+const next = base.spec({
+    whoseMove: nextWhoseMove,
+    board: nextBoard,
+    move: clear,
+    enpassant: nextEnpassant
+})
+
+
+const enqueueMove = state => move => validMove(state)(move)
+    ? base.merge(state)({ move })
+    : state
+
+
+const getBoardString = board => '----------------------\n' + board.map(row => row.join(', ')).join('\n') + '\n' + '----------------------\n'
+const printBoard = board => console.log(getBoardString(board))
+
+// printBoard(initialState.board)
+
+// console.log(initialState.board)
+// console.log(board.areDifferentTeam(initialState)(0, 0)(0, 6))
+// printBoard(pieces.king(initialState)(2, 3)(board.ZEROS))
+// printBoard(pieces.getAttackedSquares(initialState)(0))
+// console.log(board.findKing(initialState)(1))
+// pawnCheckForwardTiles(initialState)(2, 1)(zeroBoard)
+
+// const read = currentState => {
+//     const move = readline.question('hi')
+//     // read(next(enqueueMove(currentState)(move)))
+// }
+// read(initialState)
+
+// readline.question('hi')
+
+const removeWhitespace = x => x.replace(/ /g, '')
+
+const moveFromTerminal = text => {
+    const arr = removeWhitespace(text).split(',')
+    return [ {i: parseInt(arr[0]), j: parseInt(arr[1])}, {i: parseInt(arr[2]), j: parseInt(arr[3])} ]
 }
+const read = currentState => {
+    console.log(getBoardString(currentState.board), 'a ', currentState.whoseMove, 'b ', currentState.enpassant, 'c ', currentState.move)
+    readline.question('q' + getBoardString(currentState.board), moveRaw => {
+        const move = moveFromTerminal(moveRaw)
+        read(next(enqueueMove(currentState)(move)))
+    })
+}
+read(initialState)
+
+// readline.question('hi', answer => {
+//     console.log('hello')
+// })
