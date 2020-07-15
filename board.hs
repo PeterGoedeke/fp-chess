b3b770a0462059630720273c5f720446147a0438
PeterGoedeke <peterbgoedeke@gmail.com>
2020-07-15 14:00:15 +1200

    Board module finished

--- board.hs
+++ board.hs
@@ -46,9 +46,23 @@ codeAt :: Board -> Point -> PieceOf
 codeAt board (x, y) = board !! x !! y
 
 -- squareIsPawn :: Board -> (Num, Num)
+teamAt, teamNotAt :: Board -> Point -> Maybe Team
+teamAt board point = case codeAt board point of
+    Piece _ t -> Just t
+    None -> Nothing
+
+teamNotAt board point = teamAt board point >>= otherTeam
+-- is there a nicer way to do this point free?
+-- teamNotAt = flip flip otherTeam . ((>>=) .) . teamAt
+
+squareIsPawn, squareIsEmpty :: Board -> Point -> Bool
 squareIsPawn board point = case (codeAt board point) of
     Piece Pawn _ -> True
     _ -> False
+
+squareIsEmpty board point = case codeAt board point of
+    Piece _ _ -> False
+    None -> True
 
 -- teamAtIs :: Board -> Point -> Team -> Bool
 -- teamAtIs board p team = case ((codeAt board p) t) of
@@ -56,12 +70,23 @@ squareIsPawn board point = case (codeAt board point) of
 --     Black -> False
 
 -- teamIs :: PieceOf -> Maybe a
-teamIs :: PieceOf -> Team -> Maybe Bool
+teamIs, teamIsnt :: PieceOf -> Team -> Maybe Bool
 teamIs (Piece _ t) team = Just $ t == team
 teamIs (None) team = Nothing
 
-teamIsnt :: PieceOf -> Team -> Maybe Bool
 teamIsnt = (notM .) . teamIs
 
-main = do 
-    putStrLn $ showBoard initialBoard
+pawnDirection :: (Num a) => PieceOf -> Maybe a
+pawnDirection (Piece _ t) = Just (if t == White then 1 else -1)
+pawnDirection (None) = Nothing
+
+pawnStart :: (Num a) => Team -> a
+pawnStart team = if team == White then 1 else 6
+
+boardFind :: Board -> PieceOf -> [Point]
+boardFind board piece = [(x,y) | (y,line) <- zip [0..] board, x <- elemIndices piece line]
+
+kingFind :: Board -> Team -> Maybe Point
+kingFind board team = case boardFind board (Piece King team) of
+    x:xs -> Just x
+    _ -> Nothing
